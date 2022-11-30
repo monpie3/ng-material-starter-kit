775eeffaf750c7e7a04ddcd1f8f152e4a26375c1
Monika <dotyk_zla@o2.pl>
2022-12-01 00:00:20 +0100

    LAB-8 EX-3  Create product details

--- src/app/components/product-detail/product-detail.component-module.ts
+++ src/app/components/product-detail/product-detail.component-module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { ProductDetailComponent } from './product-detail.component';
-import {AsyncPipe} from "@angular/common";
+import {AsyncPipe, NgIf} from "@angular/common";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
-    imports: [
-        AsyncPipe
-    ],
+  imports: [
+    AsyncPipe,
+    MatCardModule,
+    NgIf
+  ],
   declarations: [ProductDetailComponent],
   providers: [],
   exports: [ProductDetailComponent]
